3d6c96e29000c660f340c79617d0b3c8c9a2d8ce
MeowGuaurr <Reynitamercedes@gmail.com>
2024-02-22 23:49:31 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+
+import diaryRouter from './routes/diaries'
+
+const app = express()
+app.use(express.json())
+
+const PORT = 3000
+
+app.get('/test', (_req, res) => {
+    console.log("someone is testing " + new Date().toLocaleDateString())
+    res.send('testing approved ' + new Date().toLocaleDateString())
+})
+
+app.use('/api/diaries', diaryRouter)
+
+app.listen(PORT,() => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ src/services/diary.ts
@@ -0,0 +1,5 @@
+import diaryData fron './diary.json'
+
+export const getEntries = () => diaryData
+
+export const addEntry = () => null
